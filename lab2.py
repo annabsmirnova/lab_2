c4890aa42bc1a01bca7fe35f35b19930c315b3f1
annabsmirnova <90455502+annabsmirnova@users.noreply.github.com>
2021-12-24 00:28:00 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,15 +1,36 @@
 import csv
 import requests
 
+FILE_ANIME = 'anime.csv'
+TOP_SIZE = 5
+
+questions = {
+    'Type': 'Какой тип аниме вас интересует?',
+    'Finished': 'Вы хотите аниме было завершено?',
+    'Studios': 'Аниме какой студии вы хотите посмотреть?',
+    'Tags': 'Какие тэги в описании аниме должны присутствовать(перечислите через запятую)?',
+    'Rating Score': 'Какой минимальный рейтинг должен быть у аниме?'
+}
+
+
+def open_file(FILE_ANIME):
+    your_anime = list(dict())
+    with open(FILE_ANIME, encoding='utf8') as f:
+        reader = csv.DictReader(f)
+        for one_anime in reader:
+            your_anime.append(one_anime)
+    f.close()
+    return your_anime
+
 
 def words_filter(anime, category, chose):
     newanime = []
     for one_anime in anime:
-        flag = 1
-        for j in chose:
-            if int(one_anime[category].split(', ').count(j)) != 1:
-                flag = 0
-        if flag != 0:
+        chose_in_anime = True
+        for one_chose in chose:
+            if int(one_anime[category].split(', ').count(one_chose)) != 1:
+                chose_in_anime = False
+        if chose_in_anime == True:
             newanime.append(one_anime)
     return newanime
 
@@ -23,81 +44,59 @@ def numbers_filter(anime, category, chose):
     return newanime
 
 
-def save(your_anime):
+def save_anime(your_anime):
     with open('your_anime.csv', 'w', encoding='utf8', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(('Anime-PlanetID', 'Name', 'Alternative Name', 'Rating Score', 'Number Votes', 'Tags', 'Content Warning', 'Type', 'Episodes', 'Finished', 'Duration', 'StartYear', 'EndYear', 'Season', 'Studios', 'Synopsis', 'Url'))
+        writer.writerow(('Anime-PlanetID', 'Name', 'Alternative Name', 'Rating Score', 'Number Votes', 'Tags',
+                         'Content Warning', 'Type', 'Episodes', 'Finished', 'Duration', 'StartYear', 'EndYear',
+                         'Season', 'Studios', 'Synopsis', 'Url'))
         for one_anime in your_anime:
-            writer.writerow((one_anime['Anime-PlanetID'], one_anime['Name'], one_anime['Alternative Name'], one_anime['Rating Score'], one_anime['Number Votes'], one_anime['Tags'], one_anime['Content Warning'], one_anime['Type'], one_anime['Episodes'], one_anime['Finished'], one_anime['Duration'], one_anime['StartYear'], one_anime['EndYear'], one_anime['Season'], one_anime['Studios'], one_anime['Synopsis'], one_anime['Url']))
+            writer.writerow((one_anime['Anime-PlanetID'], one_anime['Name'], one_anime['Alternative Name'],
+                             one_anime['Rating Score'], one_anime['Number Votes'], one_anime['Tags'],
+                             one_anime['Content Warning'], one_anime['Type'], one_anime['Episodes'],
+                             one_anime['Finished'], one_anime['Duration'], one_anime['StartYear'], one_anime['EndYear'],
+                             one_anime['Season'], one_anime['Studios'], one_anime['Synopsis'], one_anime['Url']))
     f.close()
 
 
-anime = list(dict())
-with open('anime.csv', encoding='utf8') as f:
-    reader = csv.DictReader(f)
-    for one_anime in reader:
-        anime.append(one_anime)
-f.close()
-
-your_anime = anime
-
-category = 'Type'
-print('Какой тип аниме вас интересует?')
-chose = input()
-chose = chose.split(', ')
-if chose != ['']:
-    your_anime = words_filter(your_anime, category, chose)
-
-category = 'Finished'
-print('Вы хотите аниме было завершено?')
-chose = input()
-if chose:
-    if chose == 'Да' or chose == 'да':
-        chose = ['True']
+def save_posters(your_anime):
+    if len(your_anime) > TOP_SIZE:
+        number_of_posters = TOP_SIZE
     else:
-        chose = ['False']
-    if chose != ['']:
-        your_anime = words_filter(your_anime, category, chose)
+        number_of_posters = len(your_anime)
+    for i in range(number_of_posters):
+        poster = requests.get(
+            'https://www.anime-planet.com/images/anime/covers/thumbs/' + your_anime[i]['Anime-PlanetID'] + '.jpg')
+        file = open(f'Poster{i + 1}.jpg', 'wb')
+        file.write(poster.content)
+        file.close()
 
-category = 'Studios'
-print('Аниме какой студии вы хотите посмотреть?')
-chose = input()
-chose = chose.split(', ')
-if chose != ['']:
-    your_anime = words_filter(your_anime, category, chose)
 
-category = 'Tags'
-print('Какие тэги в описании аниме должны присутствовать(перечислите через запятую)?')
-chose = input()
-chose = chose.split(', ')
-if chose != ['']:
-    your_anime = words_filter(your_anime, category, chose)
+your_anime = open_file(FILE_ANIME)
 
-category = 'Rating Score'
-print('Какой минимальный рейтинг должен быть у аниме?')
-chose = input()
-chose = chose.split(', ')
-if chose != ['']:
-    your_anime = numbers_filter(your_anime, category, chose)
-save(your_anime)
+for category, question in questions.items():
+    print(question)
+    chose = input()
+    chose = chose.split(', ')
+    if category == 'Type':
+        if chose != ['']:
+            your_anime = words_filter(your_anime, category, chose)
+    elif category == 'Finished':
+        chose = chose[0]
+        chose = ['True'] if chose.lower() == 'да' else ['False']
+        if chose != ['']:
+            your_anime = words_filter(your_anime, category, chose)
+    elif category == 'Studios':
+        if chose != ['']:
+            your_anime = words_filter(your_anime, category, chose)
+    elif category == 'Tags':
+        if chose != ['']:
+            your_anime = words_filter(your_anime, category, chose)
+    elif category == 'Rating Score':
+        if chose != ['']:
+            your_anime = numbers_filter(your_anime, category, chose)
 
-if len(your_anime) > 5:
-    number_of_posters = 5
-else:
-    number_of_posters = len(your_anime)
-for i in range(number_of_posters):
-    poster = requests.get('https://www.anime-planet.com/images/anime/covers/thumbs/' + your_anime[i]['Anime-PlanetID'] + '.jpg')
-    if i == 0:
-        file = open('Poster1.jpg', 'wb')
-    elif i == 1:
-        file = open('Poster2.jpg', 'wb')
-    elif i == 2:
-        file = open('Poster3.jpg', 'wb')
-    elif i == 3:
-        file = open('Poster4.jpg', 'wb')
-    elif i == 4:
-        file = open('Poster5.jpg', 'wb')
-    file.write(poster.content)
-    file.close()
+save_anime(your_anime)
+save_posters(your_anime)
+
 print('Список подходящих вам аниме находятся в файле your_anime.csv, также сохранены постеры к первым 5 аниме')
-
